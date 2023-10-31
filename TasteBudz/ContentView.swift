e77576d5c57c66c8aa77fab069df3045b1c97ce3
lli205 <116689575+lli205@users.noreply.github.com>
2023-10-31 13:42:08 -0500

    Initial Commit

---
+++ TasteBudz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TasteBudz
+//
+//  Created by Litao Li on 10/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TasteBudz/TasteBudzApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasteBudzApp.swift
+//  TasteBudz
+//
+//  Created by Litao Li on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct TasteBudzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
