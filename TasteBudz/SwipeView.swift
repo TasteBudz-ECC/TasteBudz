9e10d9274574eecae90a9df96662d58fdb820176
lli205 <lli205@illinois.edu>
2023-11-03 14:49:57 -0500

    Fixeds the Google Services File and Started on the SwipeView

---
+++ TasteBudz/SwipeView.swift
@@ -0,0 +1,18 @@
+//
+//  SwipeView.swift
+//  TasteBudz
+//
+//  Created by Litao Li on 11/3/23.
+//
+
+import SwiftUI
+
+struct SwipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwipeView()
+}
