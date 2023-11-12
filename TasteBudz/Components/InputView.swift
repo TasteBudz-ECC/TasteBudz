36a9900f5aa3c5e4aa48ac2fbf33230dc152ded5
joannarodriguez134 <140280268+joannarodriguez134@users.noreply.github.com>
2023-11-12 16:17:02 -0600

    testing

---
+++ TasteBudz/Components/InputView.swift
@@ -0,0 +1,18 @@
+//
+//  InputView.swift
+//  TasteBudz
+//
+//  Created by student on 11/12/23.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputView()
+}
---
+++ TasteBudz/Core/View Model/AuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthViewModel.swift
+//  TasteBudz
+//
+//  Created by student on 11/12/23.
+//
+
+import Foundation
