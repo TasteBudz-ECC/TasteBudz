92cbd19d05a10715d0adbce9845dbd2dc30cc7f7
jesschnn <jechn34@gmail.com>
2023-12-01 17:15:52 -0500

    fixed curly bracket issues

--- TasteBudz/Core/Feed/View/FeedView.swift
+++ TasteBudz/Core/Feed/View/FeedView.swift
@@ -7,25 +7,6 @@
 
 import SwiftUI
 
-//extension String {
-//    func load() -> UIImage {
-//        
-//        
-//        do{
-//            guard let url = URL(string: self) else {
-//                return UIImage()
-//            }
-//            
-//            let data: Data = try Data(contentsOf: url)
-//       
-//            return UIImage(data: data)
-//                ?? UIImage()
-//        } catch {
-//            
-//        }
-//        return UIImage()
-//    }
-//}
 
 struct FeedView: View {
     var imageURL = URL(string:"https://images.prismic.io/bar-louie%2F28acb893-a2eb-4542-b063-d3c0cb3eb94c_739143_495794_1518558828478.jpg")
@@ -49,10 +30,10 @@ struct FeedView: View {
                                     .scaledToFit()
                                     .aspectRatio(contentMode: .fill)
                                     .frame(width:150, height: 200)
-//                                    .border(Color.gray, width:5)
+                                //                                    .border(Color.gray, width:5)
                                     .cornerRadius(20)
-//
-                               
+                                //
+                                
                             } placeholder: {
                                 ProgressView()
                             }
@@ -75,46 +56,48 @@ struct FeedView: View {
                 }
                 Rectangle()
                     .frame(width:400, height: 5)
-//                 Text("See what everyone is up to!")
-//                 // View of posts by users
-//                 VStack(spacing: 20){
-//                     ForEach(1..<10) {
-//                         Text("Note \($0)")
-//                             .foregroundStyle(.white)
-//                             .font(.largeTitle)
-//                             .frame(width: 350, height: 200)
-//                             .background(.gray)
-//                     }
-=======
-        NavigationStack {
-            ScrollView(showsIndicators: false) {
-                LazyVStack {
-                    ForEach(0 ... 10, id: \.self) { note in
-                        NoteCell()
-                        
+                //                 Text("See what everyone is up to!")
+                //                 // View of posts by users
+                //                 VStack(spacing: 20){
+                //                     ForEach(1..<10) {
+                //                         Text("Note \($0)")
+                //                             .foregroundStyle(.white)
+                //                             .font(.largeTitle)
+                //                             .frame(width: 350, height: 200)
+                //                             .background(.gray)
+                //                     }
+                NavigationStack {
+                    ScrollView(showsIndicators: false) {
+                        LazyVStack {
+                            ForEach(0 ... 10, id: \.self) { note in
+                                NoteCell()
+                                
+                            }
+                        }
                     }
+                    .refreshable {
+                        print("DEBUG: Refresh notes")
+                    }
+                    .navigationBarTitle("Notes")
+                    .navigationBarTitleDisplayMode(.inline)
                 }
-            }
-            .refreshable {
-                print("DEBUG: Refresh notes")
-            }
-            .navigationBarTitle("Notes")
-            .navigationBarTitleDisplayMode(.inline)
-        }
-        .toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button {
-                    
-                } label: {
-                    Image(systemName: "arrow.counterclockwise")
-                        .foregroundColor(.black)
-
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrow.counterclockwise")
+                                .foregroundColor(.black)
+                            
+                        }
+                    }
                 }
             }
         }
     }
     
 }
+        
 
 struct FeedView_Previews: PreviewProvider {
     static var previews: some View {
