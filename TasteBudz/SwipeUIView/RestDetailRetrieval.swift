9ed780bf5a2a2caf5ef814b80d8f0604795295f8
jesschnn <jechn34@gmail.com>
2023-12-12 19:35:29 -0500

    DEVIN SLAYYY correct images are now displaying on feedview

--- TasteBudz/SwipeUIView/RestDetailRetrieval.swift
+++ TasteBudz/SwipeUIView/RestDetailRetrieval.swift
@@ -40,3 +40,62 @@ func restDetailRetrieval(businessID: String, toRetrieve: String, completion: @es
         }
     }.resume()
 }
+
+//func restDetailRetrieval(businessID: String, toRetrieve: String /*completion: @escaping (String?*/) -> String {
+//
+//    let endpoint = URL(string: "https://api.yelp.com/v3/businesses/\(businessID)")!
+//
+//    var request = URLRequest(url: endpoint)
+//    request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+//    
+//    var result = ""
+//
+//    URLSession.shared.dataTask(with: request) { (data, response, error) in
+//        if let error = error {
+//            print("Error: \(error)")
+//            return
+//        }
+//
+//        if let data = data {
+//            do {
+//                let json = try JSONSerialization.jsonObject(with: data, options: [])
+//                let businessData = json as? [String: Any]
+//                result = (businessData?[toRetrieve] as? String)!
+//                    print("Business \(toRetrieve): \(result)")
+//               
+//                
+//            } catch {
+//                print("Error decoding JSON: \(error)")
+//                return
+//            }
+//        } else {
+//            return
+//        }
+//    }.resume()
+//    
+//    return result
+//}
+
+//func restDetailRetrieval(businessID: String, toRetrieve: String) async throws -> String? {
+//    let endpoint = URL(string: "https://api.yelp.com/v3/businesses/\(businessID)")!
+//
+//    var request = URLRequest(url: endpoint)
+//    request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+//
+//    do {
+//        let (data, _) = try await URLSession.shared.data(from: endpoint) // Pass URL directly
+//        let json = try JSONSerialization.jsonObject(with: data, options: [])
+//
+//        if let businessData = json as? [String: Any],
+//           let result = businessData[toRetrieve] as? String {
+//            print("Business \(toRetrieve): \(result)")
+//            return result
+//        } else {
+//            print("returned nil")
+//            return nil
+//        }
+//    } catch {
+//        print("Error: \(error)")
+//        throw error
+//    }
+//}
